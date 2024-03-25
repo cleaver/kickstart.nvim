f2aaef7bbdc571d37418bd5a95cbbdc8a5729066
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2024-03-24 22:46:07 -0400

    set node location for copilot

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,7 +3,15 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  { 'github/copilot.vim' },
+  {
+    'github/copilot.vim',
+    config = function()
+      vim.cmd [[
+        let g:node_host_prog = '~/.nvm/versions/node/v20.10.0/lib/node_modules'
+        let g:copilot_node_command = '~/.nvm/versions/node/v20.10.0/bin/node'
+      ]]
+    end,
+  },
   {
     'vim-test/vim-test',
     config = function()
