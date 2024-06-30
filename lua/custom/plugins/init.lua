ef299f299107cffc2211a83a9f051f8c97f9693b
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2024-06-29 22:29:40 -0400

    move colorscheme to /custom

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -146,4 +146,22 @@ return {
     -- { '<leader>tF', '<cmd>TSToolsFileReferences<cr>', desc = 'Rename [F]ile' },
     -- },
   },
+  {
+    'ellisonleao/gruvbox.nvim',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('gruvbox').setup {
+        contrast = 'hard',
+        overrides = {
+          Function = { link = 'GruvboxGreen' },
+        },
+      }
+      -- Load the colorscheme here
+      vim.cmd.colorscheme 'gruvbox'
+    end,
+  },
+  {
+    'olimorris/onedarkpro.nvim',
+  },
 }
